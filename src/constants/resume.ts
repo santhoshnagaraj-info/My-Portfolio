786d07840f47d5902117e2d87a807a5d1a4db0c4
SANTHOSH N <santhoshdeveloper.mail@gmail.com>
2026-06-18 22:14:47 +0530

    Rechecking commit to resume download.

--- src/constants/resume.ts
+++ src/constants/resume.ts
@@ -1,6 +1,6 @@
 // src/constants/resume.ts
 
 export const RESUME = {
-  file: "/resume.pdf",
-  downloadName: "Santhosh_FullStack_Resume.pdf",
+  file: "/Resume.docx",
+  downloadName: "Santhosh_FullStack_Resume.docx",
 };
